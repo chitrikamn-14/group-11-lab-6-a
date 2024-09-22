602521a2d29a9f43679253d6267615a7dca887a4
chitrikamn-14 <ee24mt018@iitdh.ac.in>
2024-09-23 00:09:51 +0530

    Added constants

--- main.c
+++ main.c
@@ -8,4 +8,7 @@ void GPIOFHandler(void);       // Interrupt handler for button presses
 void dual_switch(void);        // Logic to change duty cycle based on button press
 void SysTickHandler(void) {}   // Placeholder for SysTick, not used in this code
 
-
+// Constants
+#define frequency 100000      // 100 kHz frequency
+#define time_period (16000000/frequency)  // Time period based on 16 MHz system clock
+#define del_duty 15           // Duty cycle change for each button press
